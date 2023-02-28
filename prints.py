902b79f5157751d7be512b7bf66a5bae206cc4cb
Zhe <42683589+ZheSun15@users.noreply.github.com>
2023-02-28 15:05:50 +0800

    add code comment

--- prints.py
+++ prints.py
@@ -3,6 +3,7 @@
 def get_executable(build, rel_path):
     build_dir = build
     # use {} to pass variable by selecting their names.
+    # similar to "some text {0}, {1}".format(VarName1, VarName2)
     returncodes.exit_with_driver_input_error(
                     "Could not find build '{build}' at {build_dir}. "
                     "Please run './build.py {build}'.".format(**locals()))
